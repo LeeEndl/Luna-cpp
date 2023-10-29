58c84f0a2c074ca82fa09546314c4ed93b68f564
LeeEndl <gavinolson71@gmail.com>
2023-10-29 01:27:04 -0500

    better thread concept

--- main.cpp
+++ main.cpp
@@ -1,33 +1,28 @@
 ﻿#include <dpp/dpp.h>
 
 dpp::cluster bot("TOKEN", dpp::i_all_intents);
-std::vector<std::pair<dpp::snowflake, std::future<void>>> request;
-std::vector<std::jthread> process;
-std::vector<std::string> commands = { "!ping", "!kick ", "!purge " };
+std::vector<std::future<void>> request;
+std::vector<dpp::snowflake> ratelimit;
+std::vector<std::string> commands = { "!ping", "!purge " };
 
-void message_create(const dpp::message_create_t& event)
+void release_command(const dpp::message_create_t& event)
 {
-	if (std::ranges::count_if(request, [&](std::pair<dpp::snowflake, std::future<void>>& req) { return event.msg.member.user_id == req.first; }) > 1) return;
-	std::async(std::launch::async,
-		(event.msg.content == "!ping") ? std::function<void()>([&event]()
+	ratelimit.emplace_back(event.msg.member.user_id);
+	event.reply("> ...", false, [&](const dpp::confirmation_callback_t& reply) {
+		std::async(std::launch::async,
+		(event.msg.content == "!ping") ? std::function<void()>([&event, &reply]()
 			{
-				event.reply("> :ping_pong: pong! *" + std::to_string(static_cast<int>((bot.rest_ping + bot.get_shard(0)->websocket_ping) * 1000)) + "ms*");
+				try {
+					dpp::message msg = std::get<dpp::message>(reply.value);
+					bot.message_edit(msg.set_content("> :ping_pong: pong! *" + std::to_string(static_cast<int>(bot.rest_ping * 1000)) + "ms*"));
+				}
+				catch (dpp::exception exc) {
+					std::cout << exc.what() << std::endl;
+				}
 			}) :
-		(event.msg.content.find("!kick ") not_eq -1) ? std::function<void()>([&event]()
-			{
-				std::string id = dpp::utility::index(event.msg.content, ' ')[1];
-				bot.user_get(dpp::snowflake(dpp::utility::trim_mention(id)), [&](const dpp::confirmation_callback_t& callback) {
-					id = std::to_string(std::get<dpp::user_identified>(callback.value).id);
-					if (callback.is_error()) event.reply("> invalid user id");
-					else {
-						(event.msg.member.get_user()->get_permission(event.msg.guild_id) & dpp::p_kick_members and
-							~std::get<dpp::user_identified>(callback.value).get_permission(event.msg.guild_id) & dpp::p_administrator) ?
-							bot.guild_member_kick(event.msg.guild_id, dpp::snowflake(stoull(id))) :
-							event.reply("> you do not have permission: `Kick Members`, or I am unable to kick this person.");
-					}
-					});
-			}) : (event.msg.content.find("!purge ") not_eq -1) ? std::function<void()>([&event]()
+			(event.msg.content.find("!purge ") not_eq -1) ? std::function<void()>([&event, &reply]()
 				{
+					dpp::message msg = std::get<dpp::message>(reply.value);
 					if (event.msg.member.get_user()->get_permission(event.msg.guild_id) & dpp::p_manage_messages)
 					{
 						std::string& provided = dpp::utility::index(event.msg.content, ' ')[1];
@@ -35,31 +30,31 @@ void message_create(const dpp::message_create_t& event)
 						bot.messages_get(event.msg.channel_id, 0, 0, 0, std::clamp(amount, 1, 100), [&](const dpp::confirmation_callback_t& callback) {
 							std::vector<dpp::snowflake> message_ids;
 							for (auto& [id, msg] : std::get<dpp::message_map>(callback.value))
-								if (msg.sent - (60 * 60 * 24 * 14) <= std::time(0)) message_ids.emplace_back(id);
+								if (msg.sent - (static_cast<time_t>(60 * 60 * 24) * 14) <= std::time(0)) message_ids.emplace_back(id);
 							(message_ids.size() < 1) ?
-								event.reply("> there are no messages within this channel, or there `14 days old`") :
-								event.reply("> deleted " + std::to_string(message_ids.size()) + " messages"), bot.message_delete_bulk(message_ids, event.msg.channel_id);
+								bot.message_edit(msg.set_content("> there are no messages within this channel, or there `14 days old`")) :
+								bot.message_edit(msg.set_content("> deleted " + std::to_string(message_ids.size()) + " messages")),
+								bot.message_delete_bulk(message_ids, event.msg.channel_id);
 							});
 					}
 				}) : std::function<void()>());
+		});
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	ratelimit.erase(std::remove_if(ratelimit.begin(), ratelimit.end(), [&event](const auto& id) {
+		return id == event.msg.member.user_id;
+		}), ratelimit.end());
 }
 
 int main()
 {
-	process.emplace_back(std::jthread([]() {
-		while (true)
-		{
-			std::this_thread::sleep_for(std::chrono::seconds(1));
-			if (not request.empty()) request.clear();
-		}
-		}));
 	bot.on_log([&](const dpp::log_t& event) {
 		std::cout << event.message << std::endl;
 		});
 	bot.on_message_create([](const dpp::message_create_t& event) {
-		if (std::none_of(commands.begin(), commands.end(), [&](std::string& command) { return event.msg.content.find(command); }) or not event.msg.webhook_id.empty() or
-			event.msg.member.get_user()->is_bot()) return;
-		request.emplace_back(std::make_pair(event.msg.member.user_id, std::async(std::launch::async, message_create, event)));
+		if (std::ranges::none_of(commands, [&](std::string& command) { return event.msg.content.find(command) not_eq -1; }) or
+			not event.msg.webhook_id.empty() or event.msg.member.get_user()->is_bot() or
+			std::ranges::count_if(ratelimit, [&](dpp::snowflake& id) { return id == event.msg.member.user_id; }) > 1) return;
+		request.emplace_back(std::async(std::launch::async, release_command, event));
 		});
 	bot.start(dpp::start_type::st_wait);
 }
