ec04761f442fe9feac687f4fa393a29c00c6ff18
LeeEndl <127639272+LeeEndl@users.noreply.github.com>
2023-11-05 01:17:42 -0500

    comprehending button functionality; 25%-ish done

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 ﻿#include <dpp/dpp.h>
-std::unique_ptr<dpp::cluster> bot = std::make_unique<dpp::cluster>("TOKEN", dpp::i_all_intents);
+std::unique_ptr<dpp::cluster> bot = std::make_unique<dpp::cluster>("MTAwNDUxNDkzNTA1OTAwNTQ3MA.GuKkx7.Z6-Pkt1HINgFOKBDg_q8TkjDBEDq8UvrorE3Qg", dpp::i_all_intents);
 std::unique_ptr<std::unordered_map<dpp::snowflake, std::future<void>>> cmd_sender = std::make_unique<std::unordered_map<dpp::snowflake, std::future<void>>>();
-std::unique_ptr<std::unordered_map<dpp::snowflake, std::future<void>>> button_sender = std::make_unique<std::unordered_map<dpp::snowflake, std::future<void>>>();
+std::unique_ptr<std::unordered_map<dpp::snowflake, std::future<void>>> btn_sender = std::make_unique<std::unordered_map<dpp::snowflake, std::future<void>>>();
 
 void button_pressed(const dpp::button_click_t& event) {
-
+	std::async(std::launch::async, std::function<void()>([&event]()
+		{
+			std::unique_ptr<std::vector<std::string>> i = std::make_unique<std::vector<std::string>>(dpp::utility::index(event.custom_id, '.'));
+			if (i->at(0) == "test") event.reply(std::format("> button pressed by <@{0}>. button made by <@{1}>", (uint64_t)event.command.member.user_id, i->at(1)));
+		}));
 	std::this_thread::sleep_for(std::chrono::seconds(1));
-	button_sender->erase(event.command.member.user_id);
+	btn_sender->erase(event.command.member.user_id);
 }
 
-std::vector<std::string> commands = { "!purge " };
+std::vector<std::string> commands = { "!purge ", "!button"};
 void release_command(const dpp::message_create_t& event)
 {
 	std::promise<dpp::message> p_reply;
@@ -31,6 +35,11 @@ void release_command(const dpp::message_create_t& event)
 						});
 					});
 			}
+			if (event.msg.content == "!button") {
+				reply.add_component(dpp::component()
+					.add_component(dpp::component().set_label("test button").set_style(dpp::cos_primary).set_id(std::format(".test.{0}", (uint64_t)event.msg.member.user_id))));
+				bot->message_edit(reply);
+			}
 		}));
 	std::this_thread::sleep_for(std::chrono::seconds(1));
 	cmd_sender->erase(event.msg.member.user_id);
@@ -41,13 +50,13 @@ int main()
 	bot->on_log(dpp::utility::cout_logger());
 	bot->on_message_create([](const dpp::message_create_t& event) {
 		if (std::ranges::none_of(commands, [&event](const std::string& command) { return event.msg.content.find(command) not_eq -1; }) or
-			std::ranges::count_if(*cmd_sender, [&event](const auto& element) { return element.first == event.msg.member.user_id; }) or
+			std::ranges::count_if(cmd_sender->begin(), cmd_sender->end(), [&event](const auto& element) { return element.first == event.msg.member.user_id; }) or
 			not event.msg.webhook_id.empty() or event.msg.member.get_user()->is_bot()) return;
 		cmd_sender->emplace(event.msg.member.user_id, std::async(std::launch::async, release_command, event));
 		});
 	bot->on_button_click([](const dpp::button_click_t& event) {
-		if (std::ranges::count_if(*button_sender, [&event](const auto& element) { return element.first == event.command.member.user_id; })) return;
-		button_sender->emplace(event.command.member.user_id, std::async(std::launch::async, button_pressed, event));
+		if (std::ranges::count_if(btn_sender->begin(), btn_sender->end(), [&event](const auto& element) { return element.first == event.command.member.user_id; })) return;
+		btn_sender->emplace(event.command.member.user_id, std::async(std::launch::async, button_pressed, event));
 		});
 	bot->start(dpp::start_type::st_wait);
 }
