f2f71d300aca75490bb8a11e75b2a01478f28e50
LeeEndl <gavinolson71@gmail.com>
2023-11-07 00:14:38 -0600

    major changes throughout the code
    slash command creation is sloppy. working to improve the legibility and size.

--- main.cpp
+++ main.cpp
@@ -1,82 +1,84 @@
 ﻿#include <dpp/dpp.h>
+std::unique_ptr<dpp::cluster> bot = std::make_unique<dpp::cluster>("MTAwNDUxNDkzNTA1OTAwNTQ3MA.______.", dpp::i_all_intents);
+std::unique_ptr<std::unordered_map<dpp::snowflake, std::future<void>>> cmd_sender = std::make_unique<std::unordered_map<dpp::snowflake, std::future<void>>>();
+std::unique_ptr<std::unordered_map<dpp::snowflake, std::future<void>>> btn_sender = std::make_unique<std::unordered_map<dpp::snowflake, std::future<void>>>();
 
 struct giveaway {
 	time_t ends;
 	dpp::snowflake host;
 	std::vector<dpp::snowflake> entries;
 	uint64_t winners;
-};
-
-std::unique_ptr<dpp::cluster> bot = std::make_unique<dpp::cluster>("MTAwNDUxNDkzNTA1OTAwNTQ3MA.______.", dpp::i_all_intents);
-std::unique_ptr<std::unordered_map<dpp::snowflake, std::future<void>>> cmd_sender = std::make_unique<std::unordered_map<dpp::snowflake, std::future<void>>>();
-std::unique_ptr<std::unordered_map<dpp::snowflake, std::future<void>>> btn_sender = std::make_unique<std::unordered_map<dpp::snowflake, std::future<void>>>();
-std::unique_ptr<std::unordered_map<short, giveaway>> _giveaway = std::make_unique<std::unordered_map<short, giveaway>>();
+}; std::unique_ptr<std::unordered_map<size_t, giveaway>> _giveaway = std::make_unique<std::unordered_map<size_t, giveaway>>();
 
 void button_pressed(const dpp::button_click_t& event) {
-	std::promise<dpp::message> p_reply;
-	event.reply(dpp::message(event.command.channel_id, dpp::embed().set_description("> ...")), [&](const dpp::confirmation_callback_t& callback) {
-		p_reply.set_value(std::get<dpp::message>(callback.value));
-		});
-	dpp::message reply = p_reply.get_future().get();
-	std::async(std::launch::async, std::function<void()>([&event, &reply]()
+	std::async(std::launch::async, std::function<void()>([&event]()
 		{
 			std::unique_ptr<std::vector<std::string>> i = std::make_unique<std::vector<std::string>>(dpp::utility::index(event.custom_id, '.'));
+			if (i->at(0) == "giveaway") {
+				giveaway gw = _giveaway->at(stoull(i->at(1)));
+				if (std::ranges::find(gw.entries, event.command.member.user_id) not_eq gw.entries.end())
+					event.reply(dpp::message("> You have already entered this giveaway!").set_flags(dpp::message_flags::m_ephemeral));
+				else gw.entries.emplace_back(event.command.member.user_id);
+				_giveaway->at(stoull(i->at(1))) = gw;
+				event.reply();
+			}
 		}));
 	std::this_thread::sleep_for(std::chrono::seconds(1));
 	btn_sender->erase(event.command.member.user_id);
 }
 
-std::vector<std::string> commands = { "!purge ", "!gcreate" };
-void release_command(const dpp::message_create_t& event)
+void release_command(const dpp::slashcommand_t& event)
 {
-	std::promise<dpp::message> p_reply;
-	event.reply(dpp::message(event.msg.channel_id, dpp::embed().set_description("> ...")), false, [&](const dpp::confirmation_callback_t& callback) {
-		p_reply.set_value(std::get<dpp::message>(callback.value));
-		});
-	dpp::message reply = p_reply.get_future().get();
-	std::async(std::launch::async, std::function<void()>([&event, &reply]()
+	std::async(std::launch::async, std::function<void()>([&event]()
 		{
-			if (event.msg.content.find(commands[0]) not_eq -1 and event.msg.member.get_user()->get_permission(event.msg.guild_id) & dpp::p_manage_messages) {
-				std::string& provided = dpp::utility::index(event.msg.content, ' ')[1];
-				int amount = (std::ranges::all_of(provided, ::isdigit)) ? stoi(dpp::utility::index(event.msg.content, ' ')[1]) : 0;
-				bot->messages_get(event.msg.channel_id, 0, event.msg.id, 0, std::clamp(amount, 1, 100), [&](const dpp::confirmation_callback_t& callback) {
-					bot->message_delete_bulk(std::get<dpp::message_map>(callback.value), event.msg.channel_id, [&event, callback, &reply](const dpp::confirmation_callback_t& callback_) {
-						(callback_.is_error()) ?
-							reply.embeds[0].set_description(std::format("> {0}", callback_.get_error().message)) :
-							reply.embeds[0].set_description(std::format("> Deleted **{0}** messages", std::get<dpp::message_map>(callback.value).size()));
-						bot->message_edit(reply);
+			if (event.command.get_command_name() == "purge" and event.command.member.get_user()->get_permission(event.command.guild_id) & dpp::p_manage_messages) {
+				event.reply(dpp::message(event.command.channel_id, dpp::embed().set_description("> ...")));
+				int amount = get<int64_t>(event.get_parameter("amount"));
+				bot->messages_get(event.command.channel_id, 0, event.command.id, 0, std::clamp(amount, 1, 100), [&](const dpp::confirmation_callback_t& mg_cb) {
+					bot->message_delete_bulk(std::get<dpp::message_map>(mg_cb.value), event.command.channel_id, [&event, mg_cb](const dpp::confirmation_callback_t& mdb_cb) {
+						std::string what = mdb_cb.is_error() ?
+							std::format("> {0}", mdb_cb.get_error().message) :
+							std::format("> Deleted **{0}** messages", std::get<dpp::message_map>(mg_cb.value).size());
+						event.edit_original_response(dpp::message(what).set_flags(dpp::message_flags::m_ephemeral));
 						});
 					});
 			}
-			if (event.msg.content == "!gcreate") {
-				giveaway gw = { time(0), event.msg.member.user_id, std::vector<dpp::snowflake>(), 1 }; /* TODO: store in _giveaway */
-				reply.components.emplace_back(dpp::component().add_component(dpp::component()
-					.set_emoji(u8"🎉")
-					.set_id(std::format(".giveaway.{0}", (uint64_t)event.msg.member.user_id))));
-
-				reply.embeds[0]
+			if (event.command.get_command_name() == "gcreate") {
+				giveaway gw = { time(0), event.command.member.user_id, std::vector<dpp::snowflake>(), 1 };
+				bot->message_create(dpp::message(event.command.channel_id, dpp::embed()
 					.set_title("prize title")
 					.set_description(std::format("prize desc. \n\nEnds: {0} \nHosted by: <@{1}> \nEntries: {2} \nWinners: {3}",
 						dpp::utility::timestamp(gw.ends, dpp::utility::tf_short_datetime), (uint64_t)gw.host, gw.entries.size(), gw.winners))
-					.set_timestamp(time(0));
-				bot->message_edit(reply);
+					.set_timestamp(time(0))).add_component(dpp::component()
+						.add_component(dpp::component()
+							.set_emoji(u8"🎉")
+							.set_id(std::format(".giveaway.{0}", _giveaway->size())))));
+				event.reply(dpp::message(std::format("> The giveaway was successfully created! ID: **{0}**", _giveaway->size())).set_flags(dpp::m_ephemeral));
+				_giveaway->emplace(_giveaway->size(), gw);
 			}
 		}));
 	std::this_thread::sleep_for(std::chrono::seconds(1));
-	cmd_sender->erase(event.msg.member.user_id);
+	cmd_sender->erase(event.command.member.user_id);
 }
 
 int main()
 {
 	bot->on_log(dpp::utility::cout_logger());
-	bot->on_message_create([](const dpp::message_create_t& event) {
-		if (std::ranges::none_of(commands, [&event](const std::string& command) { return event.msg.content.find(command) not_eq -1; }) or
-			std::ranges::count_if(cmd_sender->begin(), cmd_sender->end(), [&event](const auto& element) { return element.first == event.msg.member.user_id; }) or
-			not event.msg.webhook_id.empty() or event.msg.member.get_user()->is_bot()) return;
-		cmd_sender->emplace(event.msg.member.user_id, std::async(std::launch::async, release_command, event));
+	bot->on_ready([](const dpp::ready_t& event) {
+		std::vector<dpp::slashcommand> cmds;
+		cmds.emplace_back(dpp::slashcommand().set_name("purge")
+			.set_description("mass delete messages")
+			.add_option(dpp::command_option(dpp::co_integer, "amount", "amount of messages to delete", true)));
+		cmds.emplace_back(dpp::slashcommand().set_name("gcreate")
+			.set_description("create a giveaway"));
+		if (dpp::run_once<struct bulk_command_create_t>()) bot->global_bulk_command_create(cmds);
+		});
+	bot->on_slashcommand([](const dpp::slashcommand_t& event) {
+		if (cmd_sender->contains(event.command.member.user_id)) return;
+		cmd_sender->emplace(event.command.member.user_id, std::async(std::launch::async, release_command, event));
 		});
 	bot->on_button_click([](const dpp::button_click_t& event) {
-		if (std::ranges::count_if(btn_sender->begin(), btn_sender->end(), [&event](const auto& element) { return element.first == event.command.member.user_id; })) return;
+		if (btn_sender->contains(event.command.member.user_id)) return;
 		btn_sender->emplace(event.command.member.user_id, std::async(std::launch::async, button_pressed, event));
 		});
 	bot->start(dpp::start_type::st_wait);
