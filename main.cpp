029738859d0d173d436a19294d8f868d214ae983
LeeEndl <gavinolson71@gmail.com>
2023-11-07 23:38:51 -0600

    minor changes throughout the code.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <dpp/dpp.h>
+#include <coroutine>
 std::unique_ptr<dpp::cluster> bot = std::make_unique<dpp::cluster>("MTAwNDUxNDkzNTA1OTAwNTQ3MA.______.", dpp::i_all_intents);
 std::unique_ptr<std::unordered_map<dpp::snowflake, std::future<void>>> cmd_sender = std::make_unique<std::unordered_map<dpp::snowflake, std::future<void>>>();
 std::unique_ptr<std::unordered_map<dpp::snowflake, std::future<void>>> btn_sender = std::make_unique<std::unordered_map<dpp::snowflake, std::future<void>>>();
@@ -13,7 +14,7 @@ struct giveaway {
 void button_pressed(const dpp::button_click_t& event) {
 	std::async(std::launch::async, std::function<void()>([&event]()
 		{
-			std::unique_ptr<std::vector<std::string>> i = std::make_unique<std::vector<std::string>>(dpp::utility::index(event.custom_id, '.'));
+			std::unique_ptr<std::vector<std::string>> i = dpp::utility::index(event.custom_id, '.');
 			if (i->at(0) == "giveaway") {
 				giveaway gw = _giveaway->at(stoull(i->at(1)));
 				if (std::ranges::find(gw.entries, event.command.member.user_id) not_eq gw.entries.end())
@@ -27,51 +28,49 @@ void button_pressed(const dpp::button_click_t& event) {
 	btn_sender->erase(event.command.member.user_id);
 }
 
-void release_command(const dpp::slashcommand_t& event)
+void release_command(const dpp::slashcommand_t& lam)
 {
+	std::unique_ptr<dpp::slashcommand_t> event = std::make_unique<dpp::slashcommand_t>(std::move(lam));
 	std::async(std::launch::async, std::function<void()>([&event]()
 		{
-			if (event.command.get_command_name() == "purge" and event.command.member.get_user()->get_permission(event.command.guild_id) & dpp::p_manage_messages) {
-				event.reply(dpp::message(event.command.channel_id, dpp::embed().set_description("> ...")));
-				int amount = get<int64_t>(event.get_parameter("amount"));
-				bot->messages_get(event.command.channel_id, 0, event.command.id, 0, std::clamp(amount, 1, 100), [&](const dpp::confirmation_callback_t& mg_cb) {
-					bot->message_delete_bulk(std::get<dpp::message_map>(mg_cb.value), event.command.channel_id, [&event, mg_cb](const dpp::confirmation_callback_t& mdb_cb) {
-						std::string what = mdb_cb.is_error() ?
+			if (event->command.get_command_name() == "purge" and event->command.member.get_user()->get_permission(event->command.guild_id) & dpp::p_manage_messages) {
+				event->reply(dpp::message(event->command.channel_id, dpp::embed().set_description("> ...")));
+				bot->messages_get(event->command.channel_id, 0, event->command.id, 0, std::clamp((int)get<int64_t>(event->get_parameter("amount")), 1, 100), [&](const dpp::confirmation_callback_t& mg_cb) {
+					bot->message_delete_bulk(std::move(std::get<dpp::message_map>(mg_cb.value)), event->command.channel_id, [&event, mg_cb](const dpp::confirmation_callback_t& mdb_cb) {
+						std::unique_ptr<std::string> what = std::make_unique<std::string>(mdb_cb.is_error() ?
 							std::format("> {0}", mdb_cb.get_error().message) :
-							std::format("> Deleted **{0}** messages", std::get<dpp::message_map>(mg_cb.value).size());
-						event.edit_original_response(dpp::message(what).set_flags(dpp::message_flags::m_ephemeral));
+							std::format("> Deleted **{0}** messages", std::get<dpp::message_map>(mg_cb.value).size()));
+						event->edit_original_response(dpp::message(std::move(*what)).set_flags(dpp::message_flags::m_ephemeral));
 						});
 					});
 			}
-			if (event.command.get_command_name() == "gcreate") {
-				giveaway gw = { time(0), event.command.member.user_id, std::vector<dpp::snowflake>(), 1 };
-				bot->message_create(dpp::message(event.command.channel_id, dpp::embed()
+			if (event->command.get_command_name() == "gcreate") {
+				giveaway gw = { time(0), event->command.member.user_id, {}, 1 };
+				bot->message_create(dpp::message(event->command.channel_id, dpp::embed()
 					.set_title("prize title")
 					.set_description(std::format("prize desc. \n\nEnds: {0} \nHosted by: <@{1}> \nEntries: {2} \nWinners: {3}",
 						dpp::utility::timestamp(gw.ends, dpp::utility::tf_short_datetime), (uint64_t)gw.host, gw.entries.size(), gw.winners))
-					.set_timestamp(time(0))).add_component(dpp::component()
-						.add_component(dpp::component()
-							.set_emoji(u8"🎉")
-							.set_id(std::format(".giveaway.{0}", _giveaway->size())))));
-				event.reply(dpp::message(std::format("> The giveaway was successfully created! ID: **{0}**", _giveaway->size())).set_flags(dpp::m_ephemeral));
+					.set_timestamp(time(0)))
+					.add_component(dpp::component().add_component(dpp::component()
+						.set_emoji(u8"🎉").set_id(std::format(".giveaway.{0}", _giveaway->size())))));
+				event->reply(dpp::message(std::format("> The giveaway was successfully created! ID: **{0}**", _giveaway->size())).set_flags(dpp::m_ephemeral));
 				_giveaway->emplace(_giveaway->size(), gw);
 			}
 		}));
 	std::this_thread::sleep_for(std::chrono::seconds(1));
-	cmd_sender->erase(event.command.member.user_id);
+	cmd_sender->erase(event->command.member.user_id);
 }
 
 int main()
 {
 	bot->on_log(dpp::utility::cout_logger());
 	bot->on_ready([](const dpp::ready_t& event) {
-		std::vector<dpp::slashcommand> cmds;
-		cmds.emplace_back(dpp::slashcommand().set_name("purge")
-			.set_description("mass delete messages")
+		std::unique_ptr<std::vector<dpp::slashcommand>> cmds = std::make_unique<std::vector<dpp::slashcommand>>();
+		cmds->emplace_back(dpp::slashcommand().set_name("purge").set_description("mass delete messages")
 			.add_option(dpp::command_option(dpp::co_integer, "amount", "amount of messages to delete", true)));
-		cmds.emplace_back(dpp::slashcommand().set_name("gcreate")
-			.set_description("create a giveaway"));
-		if (dpp::run_once<struct bulk_command_create_t>()) bot->global_bulk_command_create(cmds);
+		cmds->emplace_back(dpp::slashcommand().set_name("gcreate").set_description("create a giveaway"));
+
+		bot->global_bulk_command_create(std::move(*cmds));
 		});
 	bot->on_slashcommand([](const dpp::slashcommand_t& event) {
 		if (cmd_sender->contains(event.command.member.user_id)) return;
